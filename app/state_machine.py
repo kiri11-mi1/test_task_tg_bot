37f09ca0a19ae10691d881da62547e5f3b4ed60c
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-05-06 22:38:31 +0700

    добавил опциональный параметр

--- app/state_machine.py
+++ app/state_machine.py
@@ -1,14 +1,14 @@
 from transitions import Machine
 
 
-class StateMachine:
+class StateMachine(object):
 
 
     states = ['start_state', 'food_size', 'payment_form', 'checking']
 
 
-    def __init__(self):
-        self.machine = Machine(model=self, states=StateMachine.states, initial='start_state')
+    def __init__(self, initial_state='start_state'):
+        self.machine = Machine(model=self, states=StateMachine.states, initial=initial_state)
         self.machine.add_transition(trigger='start_dialog', source='start_state', dest='food_size')
         self.machine.add_transition(trigger='select_size', source='food_size', dest='payment_form')
         self.machine.add_transition(trigger='select_payment_form', source='payment_form', dest='checking')
