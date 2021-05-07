9b21d092e169263e9633c17a7290d402695ad93e
kiri11_mi1 <kirill.mil.0310@mail.ru>
2021-05-07 14:02:00 +0700

    fux

--- app/bot.py
+++ app/bot.py
@@ -1,80 +1,17 @@
 from config import TOKEN
-from state_machine import StateMachine
 from telebot import TeleBot
+from handler import get_response
 
 
 bot = TeleBot(TOKEN)
 
-# Хранилище состояний разных пользователей
-storage = dict()
 
-
-def get_or_create_state(chat_id):
-    try:
-        return storage[chat_id]['state'].state
-    except KeyError:
-        storage.update({
-            chat_id: {
-                'state': StateMachine(),
-                'size': None,
-                'payment': None,
-            }
-        })
-        return storage[chat_id]['state'].state
-
-
-@bot.message_handler(commands=["start"], func=lambda msg: get_or_create_state(msg.chat.id) == 'start_state')
+@bot.message_handler()
 def cmd_start(message):
-    storage[message.chat.id]['state'].start_dialog()
     bot.send_message(
         message.chat.id,
-        'Какую вы хотите пиццу? Большую или маленькую?'
+        get_response(message.chat.id, message.text.lower())
     )
-
-
-@bot.message_handler(commands=["reset"])
-def cmd_cancel(message):
-    storage[message.chat.id]['state'].cancel()
-    bot.send_message(message.chat.id, 'Начните заказ по новой с команды /start.')
-
-
-@bot.message_handler(func=lambda msg: get_or_create_state(msg.chat.id) == 'food_size')
-def food_size(message):
-    if message.text.lower() in ['большую', 'маленькую']:
-        storage[message.chat.id]['size'] = message.text.lower()
-        storage[message.chat.id]['state'].select_size()
-        return bot.send_message(
-            message.chat.id,
-            'Как будете оплачивать? Наличкой или безналичкой?',
-        )
-    bot.send_message(message.chat.id, 'Введите размер: большую или маленькую.')
-
-
-@bot.message_handler(func=lambda msg: get_or_create_state(msg.chat.id) == 'payment_form')
-def payment_form(message):
-    if message.text.lower() in ['наличкой', 'безналичкой']:
-        storage[message.chat.id]['payment'] = message.text.lower()
-        storage[message.chat.id]['state'].select_payment_form()
-        return bot.send_message(
-            message.chat.id,
-            f"Вы хотите {storage[message.chat.id]['size']} пиццу, оплата {storage[message.chat.id]['payment']}?",
-        )
-    bot.send_message(message.chat.id, 'Введите способ оплаты: наличкой или безналичкой.')
-
-
-@bot.message_handler(func=lambda msg: get_or_create_state(msg.chat.id) == 'checking')
-def fix_order(message):
-    if message.text.lower() == 'да':
-        storage[message.chat.id]['state'].fix_order()
-        return bot.send_message(message.chat.id, 'Спасибо за заказ!')
-    elif message.text.lower() == 'нет':
-        return cmd_cancel(message)
-    bot.send_message(message.chat.id, 'Ответьте: Да или Нет.')
-
-
-@bot.message_handler(func=lambda msg: get_or_create_state(msg.chat.id) == 'start_state')
-def default_answer(message):
-    bot.send_message(message.chat.id, 'Сделайте заказ с помощью команды /start.')
 
 
 if __name__ == '__main__':
---
+++ app/handler.py
@@ -0,0 +1,58 @@
+from state_machine import StateMachine
+
+
+storage = {}
+
+
+def get_or_create_state(chat_id):
+    try:
+        return storage[chat_id]['state'].state
+    except KeyError:
+        storage.update({
+            chat_id: {
+                'state': StateMachine(),
+                'size': None,
+                'payment': None,
+            }
+        })
+        return storage[chat_id]['state'].state
+
+
+def get_response(chat_id, text):
+    if get_or_create_state(chat_id) == 'start_state' and text == '/start':
+        storage[chat_id]['state'].start_dialog()
+        return 'Какую вы хотите пиццу? Большую или маленькую?'
+
+    elif text == '/reset':
+        storage[chat_id]['state'].cancel()
+        return 'Начните заказ по новой с команды /start.'
+
+    elif get_or_create_state(chat_id) == 'food_size':
+        if text in ['большую', 'маленькую']:
+            storage[chat_id]['size'] = text
+            storage[chat_id]['state'].select_size()
+            return 'Как будете оплачивать? Наличкой или безналичкой?'
+        else:
+            return 'Введите размер: большую или маленькую.'
+
+    elif get_or_create_state(chat_id) == 'payment_form':
+        if text in ['наличкой', 'безналичкой']:
+            storage[chat_id]['payment'] = text
+            storage[chat_id]['state'].select_payment_form()
+            return f"Вы хотите {storage[chat_id]['size']} пиццу, оплата {storage[chat_id]['payment']}?",
+        else:
+            return 'Введите способ оплаты: наличкой или безналичкой.'
+
+    elif get_or_create_state(chat_id) == 'checking':
+        if text == 'да':
+            storage[chat_id]['state'].fix_order()
+            return 'Спасибо за заказ!'
+        elif text == 'нет':
+            storage[chat_id]['state'].cancel()
+            return 'Начните заказ по новой с команды /start.'
+        else:
+            return 'Ответьте: Да или Нет.'
+
+    return 'Сделайте заказ с помощью команды /start.'
+
+
